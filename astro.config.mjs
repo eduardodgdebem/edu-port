d06d81aca8553469010a9ce0ff68f4def2d07211
eduardodgdebem <bemeduardode@gmail.com>
2023-11-13 17:28:50 +0100

    fix .env and build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()]
 });
+
