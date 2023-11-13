aa41a557f0a3667953d7b0d0e45a1e37ef3b64e9
eduardodgdebem <bemeduardode@gmail.com>
2023-11-13 16:32:21 +0100

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "vanila-bright": "#FFFFF0",
+      vanila: "#d4d4c5",
+      "vanila-dark": "#b3b3a8",
+      purple: "#7220ff",
+	  green: "#59eb15",
+      gray: "#3d424f",
+      "gray-dark": "#23262d",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
