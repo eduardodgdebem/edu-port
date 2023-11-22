140e8d2b06ce78f08911355fc3a27e43d2b4758a
eduardodgdebem <bemeduardode@gmail.com>
2023-11-22 12:13:52 +0100

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import dotenv from 'dotenv';
 import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
 dotenv.config();
 
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   adapter: vercel()
 });
